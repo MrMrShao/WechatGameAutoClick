9121fa9279cd8578917e8467ede00784640c5845
Mr Wang <whiup888@gmail.com>
2018-06-27 20:36:37 +0800

    2018-6-27-ver2

--- README.md
+++ README.md
@@ -2,7 +2,38 @@
 微信小游戏《加减大师》辅助工具，抓包+模拟点击
 
 ##游戏介绍
-《加减大师》是一款微信小游戏，玩家在指定时间内判断等式的对错，答对越多得分越多，每周得分的前500名可以兑换娃娃等礼品。
+《加减大师》是一款微信小游戏，玩家在指定时间内判断等式的对错，答对越多得分越多，每周得分的前500名可以兑换娃娃等礼品。<br>
+<div align=center>
 <img width="150" height="250" src="https://github.com/tiantianwahaha/WechatGameAutoClick/raw/master/img/example1.png"/>
 <img width="150" height="250" src="https://github.com/tiantianwahaha/WechatGameAutoClick/raw/master/img/example2.png"/>
 <img width="150" height="250" src="https://github.com/tiantianwahaha/WechatGameAutoClick/raw/master/img/example3.png"/>
+</div>
+
+**WechatGameAutoClick**是用python3实现的一个简单脚本。<br>
+不同于其他的截图识别题目然后模拟点击的脚本，本程序通过Charles软件抓包获得题目和答案，通过ADB模拟点击，60行代码就基本搞定。
+
+##效果展示
+
+##使用方法
+
+* 下载代码到本地
+```
+
+```
+* 安装所需要的包
+```
+用于监控抓包获得的数据的变化情况
+pip install watchdog
+```
+
+* 安装配置Charles
+* 配置ADB
+* 运行程序
+```
+python get_answer.py
+注意，我的手机为小米6，屏幕尺寸为1920*1080，所以我的手机对号和错号的中心位置坐标分别为(270,1650),(810,1650)
+如果屏幕尺寸不一致，请修改为合适的大小。
+```
+
+##参考
+知乎答题王（头脑王者）全自动答题辅助[wechat_brain](https://github.com/251321639/wechat_brain)
--- get_answer.py
+++ get_answer.py
@@ -21,17 +21,19 @@ def adb_tap(option):
     os.system('adb shell input tap {0} {1}'.format(conf[option]['x'], conf[option]['y']))
 
 
-def read_question(f):
-    time.sleep(1)
-    response = json.load(f)
-    f.close()
-    os.remove('D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\sprint_game')
+def read_question(response):
+    global sleep_time
+    global start
+    sleep_time = response[str(0)]['seconds'] / 8
     for i in range(40):
+        if start:
+            start = False
+            time.sleep(2)
         is_true = response[str(i)]['is_true']
-        #time.sleep(0.5)
         adb_tap('option' + str(is_true))
+        if i != 39:
+            time.sleep(sleep_time)
         print(i)
-    return response
 
 
 class FileEventHandler(FileSystemEventHandler):
@@ -39,19 +41,26 @@ class FileEventHandler(FileSystemEventHandler):
         FileSystemEventHandler.__init__(self)
 
     def on_created(self, event):
-        print('go')
-        global answer
-        if event.src_path.split('\\')[-1] == 'sprint_game':
-            with open('D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\sprint_game', encoding='utf-8') as f:
-                answer = read_question(f)
-            print(f)
+        global sleep_time
+        if os.path.exists(path):
+            with open(path) as f:
+                time.sleep(sleep_time)
+                print(sleep_time)
+                response = json.load(f)
+                f.close()
+                os.remove(path)
+                read_question(response)
 
 
 if __name__ == "__main__":
-    os.remove('D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\sprint_game')
+    path = 'D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\sprint_game'
+    sleep_time = 5 / 8
+    start = True
+    if os.path.exists(path):
+        os.remove(path)
     observer = Observer()
     event_handler = FileEventHandler()
-    observer.schedule(event_handler, r"D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\\", True)
+    observer.schedule(event_handler, r'D:\ADB\question\jjds.iwillgo.cn\index\index_one_nine_six\\', True)
     print('助手已经运行，请开始游戏')
     observer.start()
     try:
